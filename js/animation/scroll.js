9230c271fda53635fbe753c0eb288b883d959809
ThinkingDobby <chiho1221@naver.com>
2023-09-01 14:58:40 +0900

    intro.html certificate section 디자인 적용 중 임시저장(1)

--- js/animation/scroll.js
+++ js/animation/scroll.js
@@ -1,10 +1,21 @@
 document.addEventListener("DOMContentLoaded", function() {
-    var downButton = document.getElementsByClassName("toAbilitySectionButton")[0];
-  
-    downButton.addEventListener("click", function() {
+    const toAbilitySectionButton = document.getElementsByClassName("toAbilitySectionButton")[0];
+    
+    toAbilitySectionButton.addEventListener("click", function() {
+        window.scrollTo({
+          top: window.innerHeight,
+          behavior: 'smooth'
+        });
+      });
+
+
+    const toCertificateSectionButton = document.getElementsByClassName("toCertificateSectionButton")[0];
+    const certificateSection = document.getElementsByClassName("certificateSection")[0];
+
+    toCertificateSectionButton.addEventListener("click", function() {
       window.scrollTo({
-        top: window.innerHeight,
+        top: certificateSection.offsetTop + certificateSection.offsetHeight,
         behavior: 'smooth'
       });
     });
-  });
+});
