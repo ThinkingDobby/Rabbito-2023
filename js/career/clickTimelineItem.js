550e6504a3d8402439d4acf70647a4334b7f8334
ThinkingDobby <chiho1221@naver.com>
2023-09-10 11:14:06 +0900

    clickTimelineItem.js 작성 중 임시저장(2)

--- js/career/clickTimelineItem.js
+++ js/career/clickTimelineItem.js
@@ -1,14 +1,16 @@
 document.addEventListener("DOMContentLoaded", function() {
     const timelineItems = document.getElementsByClassName("item")
     const timelineLines = document.getElementsByClassName("line")
+    const contentSections = document.getElementsByClassName("section")
 
     const groups = [
-        [timelineItems[0], timelineItems[5], timelineLines[0]],
-        [timelineItems[1], timelineItems[2], timelineLines[1]],
-        [timelineItems[3], timelineItems[4], timelineLines[2]]
+        [timelineItems[0], timelineItems[5], timelineLines[0], contentSections[0]],
+        [timelineItems[1], timelineItems[2], timelineLines[1], contentSections[1]],
+        [timelineItems[3], timelineItems[4], timelineLines[2], contentSections[2]]
     ];
 
 
+    // timeline
     let clickedGroup = null;
 
     for (let group of groups) {
@@ -16,15 +18,19 @@ document.addEventListener("DOMContentLoaded", function() {
             // 마우스 오버 시
             item.addEventListener("mouseover", function() {
                 for (let member of group) {
-                    member.style.opacity = "1";
+                    if (member.className != "section") {
+                        member.style.opacity = "1";
+                    }
                 }
             });
 
-            // 마우스 아웃 시 (투명도를 원래대로 복구. 단, 클릭된 그룹은 제외)
+            // 마우스 아웃 시 (opacity, display를 원래대로 복구. 단, 클릭된 그룹은 제외)
             item.addEventListener("mouseout", function() {
                 if (clickedGroup !== group) {
                     for (let member of group) {
-                        member.style.opacity = "0.5";
+                        if (member.className != "section") {
+                            member.style.opacity = "0.5";
+                        }
                     }
                 }
             });
@@ -33,14 +39,22 @@ document.addEventListener("DOMContentLoaded", function() {
             item.addEventListener("click", function() {
                 if (clickedGroup) {
                     for (let prevMember of clickedGroup) {
-                        prevMember.style.opacity = "0.5";
+                        if (prevMember.className != "section") {
+                            prevMember.style.opacity = "0.5";
+                        } else {
+                            prevMember.style.display = "none";
+                        }
                     }
                 }
 
-                // 현재 클릭된 그룹을 저장하고 그룹의 투명도를 100%로 설정
+                // 현재 클릭된 그룹을 저장하고 그룹의 opacity를 100%로 설정 (section은 대신에 display 조정)
                 clickedGroup = group;
                 for (let member of group) {
-                    member.style.opacity = "1";
+                    if (member.className != "section") {
+                        member.style.opacity = "1";
+                    } else {
+                        member.style.display = "block";
+                    }
                 }
             });
         }
