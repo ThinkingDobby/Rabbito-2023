27bed2e2fee11a3c6dba8b7b4697917a84eb451e
ThinkingDobby <chiho1221@naver.com>
2023-09-27 16:31:12 +0900

    works.html JS 적용

--- js/works/clickButton.js
+++ js/works/clickButton.js
@@ -2,20 +2,58 @@ document.addEventListener("DOMContentLoaded", function() {
     const circleButtons = document.getElementsByClassName("circleButton")
     const arrowButtons = document.getElementsByClassName("arrowButton")
     const circleButtonGroups = document.getElementsByClassName("buttonGroup")
-    const titles = document.getElementsByClassName("subTitle")
     const contentSections = document.getElementsByClassName("section")
+    const titles = document.getElementsByClassName("textButton")
 
     const whiteCirclePath = "../data/images/works/works_circle_white.png"
     const grayCirclePath = "../data/images/works/works_circle_gray.png"
 
     let selectedType = 0;
+    let clickedIndex = 0;
+
     let firstGroupSize = document.querySelectorAll('.buttonGroup:nth-child(1) .circleButton').length;
     let secondGroupSize = document.querySelectorAll('.buttonGroup:nth-child(2) .circleButton').length;
 
-    let clickedIndex = 0;
-
     // titleButton
+    console.log(titles);
+    for (let i = 0; i < titles.length; i++) {
+        titles[i].addEventListener("mouseover", function() {
+            titles[i].style.color = "#FEFEFE";
+        });
+    
+        titles[i].addEventListener("mouseout", function() {
+            if (i != selectedType) {
+                titles[i].style.color = "#8B8B8B";
+            }
+        });
+    
+        titles[i].addEventListener("click", function() {            
+            if (i != selectedType) {
+                titles[selectedType].style.color = "#8B8B8B";
+                titles[i].style.color = "#FEFEFE";
 
+                circleButtons[clickedIndex].src = grayCirclePath;
+                contentSections[clickedIndex].style.display = "none";
+
+                circleButtonGroups[selectedType].style.display = "none";
+                circleButtonGroups[i].style.display = "flex";
+
+                if (i == 0) {
+                    circleButtons[0].src = whiteCirclePath;
+                    contentSections[0].style.display = "flex";
+
+                    clickedIndex = 0;
+                } else if (i == 1) {
+                    circleButtons[firstGroupSize].src = whiteCirclePath;
+                    contentSections[firstGroupSize].style.display = "flex";
+        
+                    clickedIndex = firstGroupSize;
+                }
+
+                selectedType = i;
+            }
+        });
+    }
 
     // arrowButton
     arrowButtons[0].addEventListener("click", function() {
@@ -23,7 +61,7 @@ document.addEventListener("DOMContentLoaded", function() {
         if (selectedType == 0) {
             if (nextIndex < 0) nextIndex = firstGroupSize - 1
         } else if (selectedType == 1) {
-            if (nextIndex < firstGroupSize) nextIndex = secondGroupSize
+            if (nextIndex < firstGroupSize) nextIndex = firstGroupSize + secondGroupSize - 1
         }
 
         circleButtons[nextIndex].dispatchEvent(new Event("click"));
@@ -31,11 +69,10 @@ document.addEventListener("DOMContentLoaded", function() {
     
     arrowButtons[1].addEventListener("click", function() {
         let nextIndex = clickedIndex + 1
-        console.log(nextIndex);
         if (selectedType == 0) {
             if (nextIndex >= firstGroupSize) nextIndex = 0
         } else if (selectedType == 1) {
-            if (nextIndex >= secondGroupSize) nextIndex = firstGroupSize
+            if (nextIndex >= firstGroupSize + secondGroupSize) nextIndex = firstGroupSize
         }
 
         circleButtons[nextIndex].dispatchEvent(new Event("click"));
